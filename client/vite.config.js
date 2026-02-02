f56ade18c966654c01714a317c70757013d9873b
vitor <vitor369789@gmail.com>
2026-02-02 13:25:43 -0300

    ajuste no proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
     server: {
       port: parseInt(env.VITE_PORT) || 3000,
       host: '0.0.0.0', // Permite acesso externo
+      allowedHosts: [
+        'adornoshmm.d36.com.br',
+        '.d36.com.br', // Permite todos os subdomínios
+        'localhost',
+      ],
       hmr: {
         clientPort: 443, // Porta HTTPS para WebSocket
         protocol: 'wss', // WebSocket seguro
