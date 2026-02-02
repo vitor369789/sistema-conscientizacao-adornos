15dfe23f97010bd9ca1c4d98bd1c6f5ea5196310
vitor <vitor369789@gmail.com>
2026-02-02 12:50:58 -0300

    ajuste css para adptação

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   ],
   theme: {
     extend: {
+      spacing: {
+        '0': '0',
+        '1': '0.175rem',
+        '2': '0.35rem',
+        '3': '0.525rem',
+        '4': '0.7rem',
+        '5': '0.875rem',
+        '6': '1.05rem',
+        '8': '1.4rem',
+        '10': '1.75rem',
+        '12': '2.1rem',
+        '16': '2.8rem',
+        '20': '3.5rem',
+        '24': '4.2rem',
+      },
       colors: {
         primary: {
           50: '#f0f9ff',
@@ -21,14 +36,14 @@ export default {
         },
       },
       fontSize: {
-        'xs': ['0.75rem', { lineHeight: '1rem' }],
-        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
-        'base': ['1rem', { lineHeight: '1.5rem' }],
-        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
-        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
-        '2xl': ['1.5rem', { lineHeight: '2rem' }],
-        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        'xs': ['0.625rem', { lineHeight: '0.875rem' }],
+        'sm': ['0.75rem', { lineHeight: '1rem' }],
+        'base': ['0.875rem', { lineHeight: '1.25rem' }],
+        'lg': ['1rem', { lineHeight: '1.5rem' }],
+        'xl': ['1.125rem', { lineHeight: '1.5rem' }],
+        '2xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+        '4xl': ['1.75rem', { lineHeight: '2.25rem' }],
       },
       animation: {
         'fade-in': 'fadeIn 0.5s ease-in',
